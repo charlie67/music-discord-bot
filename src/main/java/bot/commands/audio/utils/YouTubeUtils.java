9eaf45b4323b2227450987fe175eeb7c6203634e
Charlie Robinson <charlierobinson080@gmail.com>
2019-09-18 01:05:14 +0100

    Move to use GetSystemEnvironmentOrDefaultValue

--- src/main/java/bot/commands/audio/utils/YouTubeUtils.java
+++ src/main/java/bot/commands/audio/utils/YouTubeUtils.java
@@ -1,5 +1,6 @@
 package bot.commands.audio.utils;
 
+import bot.utils.GetSystemEnvironmentOrDefaultValue;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
@@ -29,22 +30,27 @@ public class YouTubeUtils
         BasicAudioPlaylist playlist = (BasicAudioPlaylist) yt.loadSearchResult(argument);
         List<AudioTrack> tracks = playlist.getTracks();
 
-        if (tracks.size() > 0) {
+        if (tracks.size() > 0)
+        {
             return tracks.get(0);
-        } else {
+        }
+        else
+        {
             return null;
         }
     }
 
     public static String getYoutubeThumbnail(AudioTrack np)
     {
-        try {
+        try
+        {
             String youtubeUrl = np.getInfo().uri;
 
             List<NameValuePair> params = URLEncodedUtils.parse(new URI(youtubeUrl), StandardCharsets.UTF_8);
             String videoID = params.get(0).getValue();
             return "http://img.youtube.com/vi/" + videoID + "/0.jpg";
-        } catch (URISyntaxException e) {
+        } catch (URISyntaxException e)
+        {
             e.printStackTrace();
             return "";
         }
@@ -54,14 +60,13 @@ public class YouTubeUtils
     {
         YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request ->
         {
-
-        }).setApplicationName("youtube-cmdline-search-sample").build();
+        }).setApplicationName("bot").build();
 
         // Define the API request for retrieving search results.
         YouTube.Search.List search = youtube.search().list("id,snippet");
 
         //set the API key
-        search.setKey(System.getenv("YOUTUBE_API_KEY"));
+        search.setKey(GetSystemEnvironmentOrDefaultValue.get("YOUTUBE_API_KEY"));
         search.setRelatedToVideoId(videoID);
         search.setMaxResults(4L);
 
