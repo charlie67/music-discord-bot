f97f19fe197b663c1e19efa4f43af9ec98946a31
Charlie Robinson <charlierobinson080@gmail.com>
2019-09-18 22:47:23 +0100

    Create jar that contains all the dependencies

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,4 @@
+./gradlew fatJar
 docker build . -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
