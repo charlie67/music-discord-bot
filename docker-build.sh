171379e6f7166a10840ce84dbfd6ee44265967bf
charlierobinson080@gmail.com <chr40@aber.ac.uk>
2019-12-21 12:46:31 +0000

    Fix build issues when moving to spring boot

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
-./gradlew fatJar
+./gradlew jar
 docker build . -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
