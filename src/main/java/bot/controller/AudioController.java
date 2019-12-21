1a8bf02e8fb4bd2a155b4f94534375a6ed0e4138
charlierobinson080@gmail.com <chr40@aber.ac.uk>
2019-12-21 11:39:54 +0000

    Add playtop to endpoint

--- src/main/java/bot/controller/AudioController.java
+++ src/main/java/bot/controller/AudioController.java
@@ -6,7 +6,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,18 +23,14 @@ public class AudioController
         this.botService = botService;
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<String> test()
-    {
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
     @PostMapping("/play")
-    public ResponseEntity<String> addNewSong(@RequestParam String argument, @RequestParam String guildId, @RequestParam String textChannelId, @RequestParam String memberId)
+    public ResponseEntity<String> addNewSong(@RequestParam boolean top, @RequestParam String argument,
+                                             @RequestParam String guildId, @RequestParam String textChannelId,
+                                             @RequestParam String memberId)
     {
         try
         {
-            VoiceChannelUtils.SearchAndPlaySong(botService.getJda(), argument, guildId, textChannelId, memberId, false, botService.getAudioPlayerManager());
+            VoiceChannelUtils.SearchAndPlaySong(botService.getJda(), argument, guildId, textChannelId, memberId, top, botService.getAudioPlayerManager());
         } catch (IllegalArgumentException e)
         {
             LOGGER.error(e);
