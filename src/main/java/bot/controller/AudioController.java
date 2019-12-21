6a8bd86ec8f4906b2496bdb2c9bcd6f25b6a5402
charlierobinson080@gmail.com <chr40@aber.ac.uk>
2019-12-21 01:33:42 +0000

    Reformat to remove unused imports

--- src/main/java/bot/controller/AudioController.java
+++ src/main/java/bot/controller/AudioController.java
@@ -1,16 +1,12 @@
 package bot.controller;
 
-import bot.commands.audio.utils.TrackScheduler;
 import bot.commands.audio.utils.VoiceChannelUtils;
 import bot.service.BotService;
-import net.dv8tion.jda.api.JDA;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +25,8 @@ public class AudioController
     }
 
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test()
+    {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -39,8 +36,7 @@ public class AudioController
         try
         {
             VoiceChannelUtils.SearchAndPlaySong(botService.getJda(), argument, guildId, textChannelId, memberId, false, botService.getAudioPlayerManager());
-        }
-        catch (IllegalArgumentException e)
+        } catch (IllegalArgumentException e)
         {
             LOGGER.error(e);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
