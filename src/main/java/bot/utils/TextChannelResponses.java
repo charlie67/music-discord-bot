c959aced18405d479778fb134d780630c739c15f
Charlie <charlierobinson080@gmail.com>
2020-04-02 22:30:58 +0100

    Start making text channel responses public static final strings in this file

---
+++ src/main/java/bot/utils/TextChannelResponses.java
@@ -0,0 +1,12 @@
+package bot.utils;
+
+public class TextChannelResponses
+{
+    public static final String NOT_CONNECTED_TO_VOICE_MESSAGE = "**You need to be in a voice channel.**";
+    public static final String NO_ARGUMENT_PROVIDED_TO_PLAY_COMMAND = "**Need to provide something to play**";
+    public static final String ERROR_LOADING_VIDEO = "**There was a problem loading that video.**";
+
+    public static final String NO_SUBREDDIT_PROVIDED = "**You need to provide a subreddit to search for.**";
+    public static final String UNABLE_TO_SEARCH_FOR_SUBREDDIT = "**Unable to search for that subreddit**";
+    public static final String UNABLE_TO_GET_POSTS_FOR_SUBREDDIT = "**Unable to get posts for that subreddit**";
+}
