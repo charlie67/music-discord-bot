2e20fde78ef6fa9037f3fd7458cd9df2060b078d
Charlie <charlierobinson080@gmail.com>
2020-04-02 22:30:39 +0100

    robust against failures at loading subreddits or finding posts

--- src/main/java/bot/commands/image/RedditSearchCommand.java
+++ src/main/java/bot/commands/image/RedditSearchCommand.java
@@ -2,8 +2,10 @@ package bot.commands.image;
 
 import bot.utils.Injector;
 import bot.utils.SystemEnv;
+import bot.utils.TextChannelResponses;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dean.jraw.ApiException;
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.http.OkHttpNetworkAdapter;
 import net.dean.jraw.http.UserAgent;
@@ -15,6 +17,8 @@ import net.dean.jraw.oauth.Credentials;
 import net.dean.jraw.oauth.OAuthHelper;
 import net.dean.jraw.pagination.DefaultPaginator;
 import net.dean.jraw.references.SubredditReference;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,6 +27,8 @@ import java.util.UUID;
 
 public class RedditSearchCommand extends Command
 {
+    private Logger LOGGER = LogManager.getLogger(RedditSearchCommand.class);
+
     @SystemEnv("REDDIT_CLIENT_ID")
     private String REDDIT_CLIENT_ID;
 
@@ -58,7 +64,7 @@ public class RedditSearchCommand extends Command
 
         if (subreddit == null || subreddit.isEmpty())
         {
-            event.getChannel().sendMessage("Provide a subreddit to search for").queue();
+            event.getChannel().sendMessage(TextChannelResponses.NO_SUBREDDIT_PROVIDED).queue();
             return;
         }
 
@@ -70,9 +76,17 @@ public class RedditSearchCommand extends Command
 
             if (subredditHashComponent.timeStored - System.currentTimeMillis() > 604800000)
             {
-                subredditHashComponent = new SubredditHashComponent(System.currentTimeMillis(), subreddit);
+                try{
+                    subredditHashComponent = new SubredditHashComponent(System.currentTimeMillis(), subreddit);
+                }
+                catch (ApiException e)
+                {
+                    event.getChannel().sendMessage(TextChannelResponses.UNABLE_TO_SEARCH_FOR_SUBREDDIT).queue();
+                    LOGGER.error("Encountered error when search for subreddit", e);
+                    return;
+                }
 
-                if (!subreddit.equals("randnsfw") && !subreddit.equals("rand"))
+                if (!subreddit.contains("rand"))
                 {
                     subredditHashMap.put(subreddit, subredditHashComponent);
                 }
@@ -80,11 +94,28 @@ public class RedditSearchCommand extends Command
         }
         else
         {
-            subredditHashComponent = new SubredditHashComponent(System.currentTimeMillis(), subreddit);
-            subredditHashMap.put(subreddit, subredditHashComponent);
+            try{
+                subredditHashComponent = new SubredditHashComponent(System.currentTimeMillis(), subreddit);
+            }
+            catch (ApiException e)
+            {
+                event.getChannel().sendMessage(TextChannelResponses.UNABLE_TO_SEARCH_FOR_SUBREDDIT).queue();
+                LOGGER.error("Encountered error when searching for subreddit", e);
+                return;
+            }
+            if (!subreddit.contains("rand"))
+            {
+                subredditHashMap.put(subreddit, subredditHashComponent);
+            }
         }
 
-        event.getChannel().sendMessage(subredditHashComponent.getNewUrlItem()).queue();
+        try {
+            event.getChannel().sendMessage(subredditHashComponent.getNewUrlItem()).queue();
+        } catch (IllegalAccessException e)
+        {
+            event.getChannel().sendMessage(TextChannelResponses.UNABLE_TO_GET_POSTS_FOR_SUBREDDIT).queue();
+            LOGGER.error("Encountered error when trying to get posts from subreddit", e);
+        }
     }
 
     class SubredditHashComponent
@@ -94,7 +125,7 @@ public class RedditSearchCommand extends Command
         private DefaultPaginator<Submission> paginator;
         private int itemCounter = 0;
 
-        SubredditHashComponent(long timeStored, String subreddit)
+        SubredditHashComponent(long timeStored, String subreddit) throws ApiException
         {
             this.timeStored = timeStored;
             this.subredditItems = new ArrayList<>();
@@ -111,8 +142,13 @@ public class RedditSearchCommand extends Command
             subredditItems.addAll(paginator.next());
         }
 
-        String getNewUrlItem()
+        String getNewUrlItem() throws IllegalAccessException
         {
+            if (subredditItems.size() == 0 )
+            {
+                throw new IllegalAccessException("No items in the subreddit to display");
+            }
+
             if (itemCounter >= subredditItems.size())
             {
                 extendList();
