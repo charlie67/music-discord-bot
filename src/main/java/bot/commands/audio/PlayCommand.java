27735dd6c6f1ea2b2a898868acf732d9c72687c0
Charlie <charlierobinson080@gmail.com>
2020-06-03 16:54:43 +0100

    add logging

--- src/main/java/bot/commands/audio/PlayCommand.java
+++ src/main/java/bot/commands/audio/PlayCommand.java
@@ -4,10 +4,14 @@ import bot.commands.audio.utils.VoiceChannelUtils;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PlayCommand extends Command
 {
     private final AudioPlayerManager playerManager;
+
+    private final Logger LOGGER = LogManager.getLogger(PlayCommand.class);
 
     public PlayCommand(AudioPlayerManager playerManager)
     {
@@ -19,6 +23,8 @@ public class PlayCommand extends Command
     @Override
     protected void execute(CommandEvent event)
     {
-        VoiceChannelUtils.SearchAndPlaySong(event.getJDA(), event.getArgs(), event.getGuild().getId(), event.getChannel().getId(), event.getMember().getId(), false, playerManager);
+        LOGGER.info("Play command triggered with message {}", event.getArgs());
+        VoiceChannelUtils.SearchAndPlaySong(event.getJDA(), event.getArgs(), event.getGuild().getId(),
+                event.getChannel().getId(), event.getMember().getId(), false, playerManager);
     }
 }
