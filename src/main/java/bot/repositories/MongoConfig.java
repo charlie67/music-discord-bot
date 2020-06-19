2c0a0a47f493ee1f609a63e24bfd7b7d90bd66e7
Charlie <charlierobinson080@gmail.com>
2020-06-19 20:50:58 +0100

    add mongo location into config

--- src/main/java/bot/repositories/MongoConfig.java
+++ src/main/java/bot/repositories/MongoConfig.java
@@ -3,17 +3,20 @@ package bot.repositories;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration
 {
+    @Value("${MONGO_LOCATION:mongodb://localhost:27017}")
+    private String MONGO_LOCATION;
 
     @Override
     public @NotNull MongoClient mongoClient()
     {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(MONGO_LOCATION);
     }
 
     @Override
