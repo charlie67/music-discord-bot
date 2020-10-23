0d09e5a9d5507b6115bda919979c908a3b07f032
Charlie Robinson <charlie@charlie.to>
2020-10-23 19:44:23 +0100

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 ./gradlew jar
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker buildx build --platform linux/amd64,linux/arm64/v7 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push .
