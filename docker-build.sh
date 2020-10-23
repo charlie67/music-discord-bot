6bcd897529801606aa6aa6da96e2c45d3e34e82a
Charlie Robinson <charlie@charlie.to>
2020-10-23 17:45:38 +0100

    Enable experimental features

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 ./gradlew jar
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
 docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
