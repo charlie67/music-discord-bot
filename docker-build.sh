bac92d75da3357a2d22f249bf5bd8b57027719a0
Charlie Robinson <charlie@charlie.to>
2020-10-23 19:07:08 +0100

    push builds

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,3 @@
 ./gradlew jar
-docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_USERNAME/discord-music-bot:test .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/discord-music-bot:test
+docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_USERNAME/discord-music-bot:test --push .
