163f22378623fa947bb39a8e9775899ba432545a
Charlie Robinson <charlie@charlie.to>
2020-10-23 17:51:17 +0100

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 ./gradlew jar
-echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+cat <<< $(jq '.+{"experimental":"enabled"}' ~/.docker/config.json) > ~/.docker/config.json
 docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
