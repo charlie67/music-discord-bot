303c584b635ab3bd924baa33da2ca2863ee30039
Charlie Robinson <charlie@charlie.to>
2020-10-23 18:00:06 +0100

    Remove docker settings changes from this file

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,4 @@
 ./gradlew jar
-cat <<< $(jq '.+{"experimental":"enabled"}' ~/.docker/config.json) > ~/.docker/config.json
 docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
