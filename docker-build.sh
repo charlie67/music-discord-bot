2153dec3ef64c9b04f5ea794dfe0ca9a88a43e80
Charlie Robinson <charlie@charlie.to>
2020-10-23 18:42:36 +0100

    Fix tag

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 ./gradlew jar
-docker buildx build --platform linux/amd64,linux/arm64 -t $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH .
+docker buildx build --platform linux/amd64,linux/arm64 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH
