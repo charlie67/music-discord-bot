6fe417f91198f6bf0ed6bf7a05729f89c227445e
Charlie Robinson <charlie@charlie.to>
2020-11-01 14:10:59 +0000

    See what this does

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 ./gradlew jar
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push .
-docker buildx build --platform linux/arm/v7 --tag $DOCKER_USERNAME/discord-music-bot-arm:$TRAVIS_BRANCH --push -f Dockerfile-arm .
+docker buildx build --platform linux/arm/v7 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push -f Dockerfile-arm .
