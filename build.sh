777c996ea89cc5196603271d0293b1460540e2f9
Charlie Robinson <charlierobinson080@gmail.com>
2021-04-08 23:45:45 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./gradlew jar
 echo $2 | docker login -u $1 --password-stdin
-docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push .
-docker buildx build --platform linux/arm/v7 --tag $DOCKER_USERNAME/discord-music-bot:${TRAVIS_BRANCH}-arm --push -f Dockerfile-arm .
+docker buildx build --platform linux/amd64 --tag $1/discord-music-bot:${GITHUB_REF##*/} --push .
+docker buildx build --platform linux/arm/v7 --tag $1/discord-music-bot:${GITHUB_REF##*/}-arm --push -f Dockerfile-arm .
