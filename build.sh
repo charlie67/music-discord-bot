ef1322e93961d6ff46df6dcd9a607e1ff161e2bc
Charlie Robinson <charlierobinson080@gmail.com>
2021-04-08 23:41:16 +0100

    Update and rename docker-build.sh to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./gradlew jar
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo $2 | docker login -u $1 --password-stdin
 docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:$TRAVIS_BRANCH --push .
 docker buildx build --platform linux/arm/v7 --tag $DOCKER_USERNAME/discord-music-bot:${TRAVIS_BRANCH}-arm --push -f Dockerfile-arm .
