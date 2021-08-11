fc7149b3af0340646f9eb4c70a88169604c5fe2f
Charlie <charlie@charlie.to>
2021-08-11 22:27:51 +0100

    I'm just a noob

--- src/main/java/bot/repositories/AliasEntityRepository.java
+++ src/main/java/bot/repositories/AliasEntityRepository.java
@@ -13,6 +13,6 @@ public interface AliasEntityRepository extends CrudRepository<AliasEntity, Strin
 
     AliasEntity findByServerIdAndName(String serverId, String name);
 
-    @Query("SELECT * FROM AliasEntity a WHERE a.serverId = :serverID AND a.name LIKE %:name%")
+    @Query(name = "SELECT * FROM AliasEntity a WHERE a.serverId = :serverId AND a.name LIKE %:name%", nativeQuery = true)
     Set<AliasEntity> findAliasEntityByNameContainingAndServerId(String serverId, String name);
 }
