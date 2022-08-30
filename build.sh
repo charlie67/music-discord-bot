6d6a522a30a166317cb69fe2d8483633b4081aad
Charlie Robinson <charlie@charlie.to>
2022-08-30 20:31:06 +0000

    Update build script (#18)
    * update
    
    * update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 echo $2 | docker login -u $1 --password-stdin
-docker buildx build --platform linux/amd64 --tag $1/discord-music-bot:${GITHUB_REF##*/} --push .
+docker buildx build --platform linux/amd64 --tag $1/discord-music-bot:${GITHUB_REF_NAME} --push .
